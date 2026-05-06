ad30cec84ee43ab271c00a132600314722be1cd6
leanderrketschmer <leanderkretschmer@icloud.com>
2026-05-06 09:24:51 +0000

    updated db migrate

--- db/migrate/002_create_hm_break_entries.rb
+++ db/migrate/002_create_hm_break_entries.rb
@@ -1,7 +1,7 @@
 class CreateHmBreakEntries < ActiveRecord::Migration[7.0]
   def change
     create_table :hm_break_entries do |t|
-      t.references :hm_work_entry, type: :integer, null: false, foreign_key: true, index: true
+      t.references :hm_work_entry, null: false, foreign_key: true, index: true
       t.datetime   :started_at, null: false
       t.datetime   :ended_at
       t.text       :notes
